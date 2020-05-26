845700e19ea4ba813af634c2b509dab5117b51d8
Malek Hammou <malekhammou2017@gmail.com>
2020-05-26 01:02:35 +0100

    Add patient feature done

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,6 +1,6 @@
 module.exports = function (err, req, res, next) {
   let emailErrorMessage = "Cette addresse est associée à un autre compte";
   if (err.message.includes(emailErrorMessage))
-    res.status(500).send(emailErrorMessage);
+    res.status(400).send(emailErrorMessage);
   else res.status(500).send("Something failed");
 };
