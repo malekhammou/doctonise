b5ccdb25531df36f5d748262a24371f1caf596b6
Malek Hammou <malekhammou2017@gmail.com>
2020-06-13 16:48:39 +0100

    Fixed blocked menu

--- frontend/src/commonComponents/menu/menu.jsx
+++ frontend/src/commonComponents/menu/menu.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import "../navbar/navbar.css";
 import NavbarNavlink from "../../components/navbar-navlink/navbar-navlink";
+import { useRef, useContext, useEffect } from "react";
+import { AppContext } from "../../context/appContext";
+
 const Menu = ({ className, items }) => {
+  const menu = useRef(null);
+  const { setMenu } = useContext(AppContext);
+  useEffect(() => {
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  function handleClickOutside(event) {
+    if (menu.current && !menu.current.contains(event.target)) {
+      setMenu(false);
+    }
+  }
+
   return (
-    <div className={className}>
+    <div ref={menu} className={className}>
       {" "}
       {items.map((item) => (
         <NavbarNavlink
